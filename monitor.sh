bb75b7462c283893628aa13e7416c5b69e484cbe
larryhq <dengdai@czhdeMacBook-Pro.local>
2014-09-10 17:26:16 +0800

    调试

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,8 @@
 redis_process=$(ps -ef | grep "redis_server.py" | grep -v grep)
 sms_process= $(ps -ef|grep "send_sms.py" | grep -v grep)
 
+echo "redis_process ======${redis_process}"
+echo "sms_process======${sms_process}"
 if [[ "x${redis_process}" == "x" ]]
 then
 	nohup /usr/bin/python3 /home/ubuntu/GitHub/data_transfer/apps/products/redis_server.py 1>&2 >>/dev/null &
