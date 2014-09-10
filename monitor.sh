e250094f0167eb4bcb8aa5bd5451fb3f0e1447f1
larryhq <dengdai@czhdeMacBook-Pro.local>
2014-09-10 16:31:29 +0800

    小修改

--- monitor.sh
+++ monitor.sh
@@ -4,12 +4,12 @@
 redis_process=$(ps -ef | grep "redis_server.py" | grep -v grep)
 sms_process= $(ps -ef|grep "send_sms.py" | grep -v grep)
 
-if [[ "x${redis_process}" == "x" ]]
+if [ "x${redis_process}" == "x" ]
 then
 	nohup /usr/bin/python3 /home/ubuntu/GitHub/data_transfer/apps/products/redis_server.py >/dev/null &
 fi
 
-if [[ "x"${sms_process} == "x" ]]
+if [ "x"${sms_process} == "x" ]
 then
 	nohup /usr/bin/python3 /home/ubuntu/GitHub/data_transfer/apps/products/send_sms.py >/dev/null &
 fi
